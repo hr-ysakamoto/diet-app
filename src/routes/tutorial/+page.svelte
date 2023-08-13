9e40456b5b977d9d5961b1198d14c85c5e31f841
hr-y.sakamoto <59268274+yuki-skmt@users.noreply.github.com>
2023-08-14 00:47:25 +0900

    feat: メニューを追加

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Tutorial</h1>
+<h1>Svelte Tutorial</h1>
 <p>This is a tutorial for Svelte.</p>
+<h2>Svelte Basic</h2>
+<ul>
+  <li>
+    <a href="tutorial/svelte/basic/introduction">Introduction</a>
+  </li>
+  <li>
+    <a href="tutorial/svelte/basic/reactivity">Reactivity</a>
+  </li>
+  <li>
+    <a href="tutorial/svelte/basic/props">Props</a>
+  </li>
+  <li>
+    <a href="tutorial/svelte/basic/logic">Logic</a>
+  </li>
+  <li>
+    <a href="tutorial/svelte/basic/events">Events</a>
+  </li>
+  <li>
+    <a href="tutorial/svelte/basic/bindings">Bindings</a>
+  </li>
+  <li>
+    <a href="tutorial/svelte/basic/bindings">Lifecycle</a>
+  </li>
+  <li>
+    Stores
+    <ul>
+      <li>
+        <a href="tutorial/svelte/basic/stores/writable">Writable store</a>
+      </li>
+      <li>
+        <a href="tutorial/svelte/basic/stores/readable">Readable store</a>
+      </li>
+      <li>
+        <a href="tutorial/svelte/basic/stores/custom">Custom store</a>
+      </li>
+      <li>
+        <a href="tutorial/svelte/basic/stores/bindings">Store bindings</a>
+      </li>
+    </ul>
+  </li>
+</ul>

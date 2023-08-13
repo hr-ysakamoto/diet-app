7b599a2bee9647c62863021db3b1b8a4e18be2af
hr-y.sakamoto <59268274+yuki-skmt@users.noreply.github.com>
2023-08-13 11:09:30 +0900

    feat: Tutorial: DOM Eventsまで

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
+export async function getRandomNumber() {
+  const sleep = (waitTime: number) =>
+    new Promise((resolve) => setTimeout(resolve, waitTime));
+  await sleep(1000);
+  return Math.floor(Math.random() * 100);
+}
---
+++ src/routes/tutorial/Nested.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  export let name: string = "default";
+</script>
+
+<p>This is nested component. | {name}</p>
